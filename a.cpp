1163525f8959d499c31bf30786386da1e0faa17e
chanme <403567116@qq.com>
2015-10-09 15:35:32 +0800

    git for test

---
+++ a.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: a.cpp
+    > Author: chanme
+    > Mail: 403567116@qq.com 
+    > Created Time: Fri 09 Oct 2015 03:34:12 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+}
